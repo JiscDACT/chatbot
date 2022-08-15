8e60118f00bf2b194a76762553e7c49677764d0d
jennifer-cooper <jennifer.cooper@bristol.ac.uk>
2022-08-15 12:09:46 +0100

    intents programmatic add from CSV

--- lex_boto3.py
+++ lex_boto3.py
@@ -27,6 +27,7 @@ client = boto3.client('lexv2-runtime', region_name='eu-west-2',
                       aws_access_key_id=AWS_KEY_ID,
                       aws_secret_access_key=AWS_SECRET)
 
+print(client)
 # https://aws.amazon.com/blogs/machine-learning/interact-with-an-amazon-lex2v2-bot-with-the-aws-cli-aws-sdk-for-python-and-aws-sdk-dotnet/
 # Interacting with your bot
 
@@ -54,6 +55,8 @@ print(json.dumps(response, indent=4, sort_keys=True))
 # -----------------------------------------------------------------------------------------------------------------------
 # Firstly create the bot
 # -----------------------------------------------------------------------------------------------------------------------
+# Only need to create the bot once
+
 client = boto3.client('lexv2-models', region_name='eu-west-2',
                       aws_access_key_id=AWS_KEY_ID,
                       aws_secret_access_key=AWS_SECRET)
@@ -76,9 +79,12 @@ print(response)
 # -----------------------------------------------------------------------------------------------------------------------
 # Create a bot Locale
 # -----------------------------------------------------------------------------------------------------------------------
+# botid of the new bot : KA6ARFQ5CT
+# Only need to create a Bot locale once
 
 response = client.create_bot_locale(
-    botId=created_bot_response['botId'],
+    # botId=created_bot_response['botId'],
+    botId='KA6ARFQ5CT',
     botVersion='DRAFT',
     localeId='en_GB',
     description='Bot locale',
@@ -128,13 +134,97 @@ response = client.create_intent(
         }
     },
 
-    botId=created_bot_response['botId'],
+    #botId=created_bot_response['botId'],
+    botId='KA6ARFQ5CT',
     botVersion='DRAFT',
     localeId='en_GB'
 
 )
 
 print(response)
+# -----------------------------------------------------------------------------------------------------------------------
+# Write a function that can add a load of intents at once, skip ones that are already on there but add new ones
+# this is what we want to update each time
+
+import pandas as pd
+
+df = pd.read_csv("intents_long.csv")
+
+
+# Here is a function that could work for it
+
+def create_intent_jen(intentname,
+                      desc,
+                      utterances,
+                      fulfillment):
+    response = client.create_intent(
+        intentName=intentname,
+        description=desc,
+        sampleUtterances=utterances,
+        dialogCodeHook={
+            'enabled': False
+        },
+        fulfillmentCodeHook={
+            'enabled': False,
+            'postFulfillmentStatusSpecification': {
+                'successResponse': {
+                    'messageGroups': [
+                        {
+                            'message': {
+                                'plainTextMessage': {
+                                    'value': fulfillment
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+
+        #botId=created_bot_response['botId'],
+        botId='KA6ARFQ5CT',
+        botVersion='DRAFT',
+        localeId='en_GB'
+
+    )
+    return response
+
+
+
+# Adding Intents and utterances from the dataset
+# Attempt for long format data
+
+
+for i in df['intentname'].unique():
+    # Get the data in the right format for the create_intent function which is fussy
+    utterances = df[df['intentname'] == i]['utterances'].tolist()
+    utterance_list = []
+
+    for j in utterances:
+        thisdict = {"utterance": j}
+        utterance_list.append(thisdict)
+
+    intentname = df[df['intentname'] == i]['intentname'].reset_index(drop=True)[0]
+    desc = df[df['intentname'] == i]['desc'].reset_index(drop=True)[0]
+    fulfillment = df[df['intentname'] == i]['fulfillment'].reset_index(drop=True)[0]
+
+    response = create_intent_jen(intentname=intentname,
+                                 desc=desc,
+                                 utterances=utterance_list,
+                                 fulfillment=fulfillment)
+
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        print("Intent Creation failed!!!\n", response)
+        break
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Check it has done it
+
+# Describe the Lex Bot
+client.describe_bot(
+    botId='KA6ARFQ5CT'
+)
+
 # -----------------------------------------------------------------------------------------------------------------------
 # Create a version? Just using DRAFT for now
 
@@ -158,3 +248,96 @@ print(response['botId'])
 
 # Resources
 # https://towardsaws.com/getting-started-with-aws-lex-using-a-datafile-and-aws-python-sdk-64517fd751b7
+
+# -----------------------------------------------------------------------------------------------------------------------
+# End
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Working
+
+
+
+utterances = df[df['intentname'] == "Agile"]['utterances'].tolist()
+utterance_list = []
+
+for j in utterances:
+        thisdict = {"utterance": j}
+        utterance_list.append(thisdict)
+
+
+
+
+
+
+
+# Adding Intents and utterances from the dataset
+# Attempt for wide format data
+
+for idx, row in df.iterrows():
+    response = create_intent(intentname=row['intentname'],
+                             desc=row['description'],
+                             utterances=row['utterances'],
+                             fulfillment=row['fulfillment'])
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        print("Creating the intents failed :(!\n", response)
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+df['intentname'].unique()
+utterances = df[df['intentname'] == 'Agile']['utterances'].values.tolist()
+print(utterances)
+
+for key in utterances:
+    utterances['utterance'] = utterances.pop(key)
+
+
+dictionary to string
+the str.replace()
+
+hello = '0:'
+
+import re
+
+utterances = str(df[df['intentname'] == 'Agile']['utterances'].to_dict())
+
+new = re.sub(
+           "[0-9]\:",
+           "utterance",
+    utterances
+       )
